e8de66c829e7c9e07a6019d44564e3f76a97a596
Danarendra <narendra.daniswara16@gmail.com>
2024-09-13 11:30:03 +0700

    BARU NEWW

--- Siakad19.java
+++ Siakad19.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             String nama, nim;
-            char kelas;
+            String kelas;
             byte absen;
-            double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+            double nilaiUjian, nilaiTugas, nilaiAkhir, nilaiKuis;
             System.out.println("Masukkan Nama: ");
             nama = sc.nextLine();
             System.out.println("Masukkan NIM: ");
             nim = sc.nextLine();
             System.out.println("Masukkan Kelas: "); 
-            kelas = sc.nextLine().charAt(0);
+            kelas = sc.nextLine();
             System.out.println("Masukkan Nomor Absen: ");
             absen = sc.nextByte();
             System.out.println("Masukkan Nilai Kuis: ");
@@ -23,8 +23,8 @@ import java.util.Scanner;
             System.out.println("Masukkan Nilai Ujian: ");
             nilaiUjian = sc.nextDouble();
             nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
-            System.out.println("Nama: " + nama + "NIM: " + nim);
-            System.out.println("Kelas: " + kelas + "Absen: " + absen);
+            System.out.println("Nama: " + nama + " NIM: " + nim);
+            System.out.println("Kelas: " + kelas + " Absen: " + absen);
             System.out.println("Nilai Akhir: " + nilaiAkhir);
 
         }
